d126723021cc4db29f52afd34469fa7e45353429
David <doro.hd@skiff.com>
2023-03-28 18:57:40 +0200

    Add cookies functionality

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-import path from 'path'
+import crossEnv from 'cross-env'
+import { createClient } from '@supabase/supabase-js'
 import express from 'express'
+import cookieParser from 'cookie-parser'
 import articles from './articles.js'
 
+// Create a single supabase client for interacting with your database
+const supabase = createClient('https://kfivnxsczsonvtyfufyb.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmaXZueHNjenNvbnZ0eWZ1ZnliIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzk5MDk2MTEsImV4cCI6MTk5NTQ4NTYxMX0.MMIe_FkIsO8K8zo9Q69yHw8V4VRARk4KpzjakXGJk3s')
+
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(express.static('public'))
 
 app.set('view engine', 'pug')
@@ -20,7 +28,33 @@ app.get('/articles/:id', (req, res) => {
   res.render('article', { article: article })
 })
 
-const port = 8080
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', (req, res) => {
+  const body = req.body
+
+  if (body.username === 'admin' && body.password === 'admin') {
+    res.cookie('username', body.username,{
+      maxAge: 60 * 60 * 3
+    })
+
+    res.redirect('/')
+  }
+
+  res.redirect('/login')
+})
+
+app.get('/admin/article', (req, res) => {
+  const cookies = req.cookies
+  console.log(cookies)
+
+  res.send(cookies)
+})
+
+const port = process.env.PORT || 8080 
 app.listen(port, () => {
   console.log('Listening on port', port)
 })
